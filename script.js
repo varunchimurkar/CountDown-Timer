72e316b2cb359b381753f5462694570823d73362
Varun Chimurkar <varunchimurkar0908@gmail.com>
2025-11-11 18:40:56 +0530

    Modify -> Script.js

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
-const startButton = document.getElementById('startbutton')
-const timeInput = document.getElementById('timeInput')
-const display = document.getElementById('countdownDisplay')
+const startButton = document.getElementById("startbutton");
+const timeInput = document.getElementById("timeInput");
+const display = document.getElementById("countdownDisplay");
 
-function startTimer(){
-    let valueInSecond = parseInt(timeInput.value)
-    
-    if(isNaN(valueInSecond)) {
-        display.innerText = 'Please enter a valid number'
-        return
+function startTimer() {
+  let valueInSecond = parseInt(timeInput.value);
+
+  if (isNaN(valueInSecond)) {
+    display.innerText = "Please enter a valid number";
+    return;
+  }
+
+  if (valueInSecond <= 0) {
+    display.innerText = "Please enter seconds greater than zero";
+    return;
+  }
+
+  const timer = setInterval(function () {
+    valueInSecond--;
+    display.innerText = `Time remaining : ${valueInSecond} seconds`;
+
+    if (valueInSecond <= 0) {
+      clearInterval(timer);
+      display.innerText = "Time's up!";
     }
-
-    if(valueInSecond <=0) {
-        display.innerText = 'Please enter seconds greater than zero'
-        return
-    }
-
-    const timer = setInterval(function() {
-        valueInSecond--;
-        display.innerText = `Time remaining : ${valueInSecond} seconds`
-           
-        if(valueInSecond <= 0) {
-            clearInterval(timer)
-            display.innerText = "Time's up!"
-        }
-        
-    }, 1 * 1000)
+  }, 1 * 1000);
 }
 
-
-startButton.addEventListener('click', startTimer)
+startButton.addEventListener("click", startTimer);
